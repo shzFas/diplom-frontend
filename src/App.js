cef4bd70b111e564702185425575799c921a741b
shzfas <yuraka200486@gmail.com>
2023-03-07 16:00:40 +0600

    feat: add component myAccount(userMe)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { Home, Registration, Login } from "./pages";
 import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 import PredmetTeacher from "./components/PredmetTeacher";
+import UserMe from "./components/UserMe";
 
 function App() {
   const dispatch = useDispatch();
@@ -28,6 +29,7 @@ function App() {
                   path="journal"
                   element={<PredmetTeacher userData={userData} />}
                 />
+                <Route path="me" element={<UserMe userData={userData} />} />
               </Route>
               <Route path="*" element={<Navigate to="/" />} />
             </Routes>
